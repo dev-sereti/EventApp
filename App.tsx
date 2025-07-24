2fcd1a87603404e1b06de61a6693ed3d44cb46e9
dev <devsereti@gmail.com>
2025-07-24 18:04:10 +0300

    JS Updated.

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import EventListScreen from './src/screens/EventListScreen';
+import EventDetailScreen from './src/screens/EventDetailScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="EventList">
+        <Stack.Screen 
+          name="EventList" 
+          component={EventListScreen}
+          options={{
+            title: 'Tukutane Events',
+            headerStyle: {backgroundColor: '#6366f1'},
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen 
+          name="EventDetail" 
+          component={EventDetailScreen}
+          options={{
+            title: 'Event Details',
+            headerStyle: {backgroundColor: '#6366f1'},
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
